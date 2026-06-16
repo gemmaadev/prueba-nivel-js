3cf178bd81bff40054d006e504e4d49fa1b58579
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-16 11:38:43 +0200

    feat: integrate validation, fizzbuzz logic, and history logs into UI v1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { renderUi } from "./main.js";
+import { renderUi } from "./src/scripts/render.js";
 
 // per correr la interface - inicialitzar la UI i els event listeners
 renderUi();
--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,3 +1,7 @@
+import { fizzBuzzLogic } from "./fizzbuzz.js";
+import { historyLog, getHistoryLog } from "./history-log.js";
+import { validateInput } from "./validations.js";
+
 // Manipulación del DOM
 // Se muestra en pantalla un historial con los valores ingresados y sus resultados.
 // Manejar errores usando `<span>try...catch</span>`. (dejarlo para el final)
@@ -16,5 +20,23 @@ export function renderUi() {
   //Escoltar el botó
   forms.addEventListener("submit", (event) => {
     event.preventDefault();
+    try {
+      const error = validateInput(userInput.value);
+
+      if (error !== null) {
+        fizzBuzzResult.innerHTML = `<li>${error}</li>`;
+        return;
+      }
+
+      const inputNumber = Number(userInput.value);
+
+      const result = fizzBuzzLogic(inputNumber);
+
+      result.innerHTML = `<li>${result.message}</li>`;
+
+      forms.reset();
+    } catch (error) {
+      fizzBuzzResult.innerHTML = `<li>Something went wrong. Please, try again</li>`;
+    }
   });
 }
