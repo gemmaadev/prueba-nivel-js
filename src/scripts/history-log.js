f63aac1ff75a09c8ca93d7befa5eb6df0679d134
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-16 11:00:39 +0200

    feat: implement history management logic in history-log.js

--- src/scripts/history-log.js
+++ src/scripts/history-log.js
@@ -1,1 +1,11 @@
 // Guardar cada número ingresado en un objeto junto con su resultado.
+
+let fizzBuzzHistory = [];
+
+export function historyLog(result) {
+  fizzBuzzHistory.push(result);
+}
+
+export function getHistoryLog() {
+  return fizzBuzzHistory;
+}
