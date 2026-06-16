cfa8ad2f5b1cf035542bbe9ec4ab5e083eda9594
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-16 12:29:07 +0200

    test: add unit test for fizzbuzz logic v1

--- src/scripts/fizzbuzz.js
+++ src/scripts/fizzbuzz.js
@@ -7,32 +7,35 @@ export function fizzBuzzLogic(userNumber) {
   const FIZZ_DIVISOR = 3;
   const BUZZ_DIVISOR = 5;
 
-  let message = "";
-  let result = "";
-
-  // Funcional: Si el número ingresado es divisible por 3 y 5, se muestra FizzBuzz.
-  if (userNumber % (FIZZ_DIVISOR * BUZZ_DIVISOR) === 0) {
-    message = "FizzBuzz";
-    result = userNumber % (FIZZ_DIVISOR * BUZZ_DIVISOR);
-  }
+  let message;
+  let result;
 
   //Si el número ingresado es divsible por 3, se muestra Fizz
   if (userNumber % FIZZ_DIVISOR === 0) {
     message = "Fizz";
+    userNumber.toString();
     result = userNumber % FIZZ_DIVISOR;
   }
 
   //Si el número ingresado es divisble por 5 se muestra Buzz
   if (userNumber % BUZZ_DIVISOR === 0) {
     message = "Buzz";
+    userNumber.toString();
     result = userNumber % BUZZ_DIVISOR;
   }
 
-  // Funcional: Si el número ingresado no es divisible ni por 3 ni por 5, se muestra el número mismo.
-  if (userNumber % FIZZ_DIVISOR !== 0 || userNumber % BUZZ_DIVISOR !== 0) {
-    message = userNumber.toString();
-    result = userNumber % FIZZ_DIVISOR || userNumber % BUZZ_DIVISOR;
+  // Funcional: Si el número ingresado es divisible por 3 y 5, se muestra FizzBuzz.
+  if (userNumber % (FIZZ_DIVISOR * BUZZ_DIVISOR) === 0) {
+    message = "FizzBuzz";
+    userNumber.toString();
+    result = userNumber % (FIZZ_DIVISOR * BUZZ_DIVISOR);
   }
+
+  //   // Funcional: Si el número ingresado no es divisible ni por 3 ni por 5, se muestra el número mismo.
+  //   if (userNumber % FIZZ_DIVISOR !== 0 || userNumber % BUZZ_DIVISOR !== 0) {
+  //     message = userNumber.toString();
+  //     result = userNumber % FIZZ_DIVISOR || userNumber % BUZZ_DIVISOR;
+  //   }
 
   return {
     message, // Mensaje
--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -1,71 +1,74 @@
 import { expect, describe, test } from "@jest/globals";
-import { fizzBuzz } from "../src/fizzBuzz";
+import { fizzBuzzLogic } from "/src/scripts/fizzbuzz.js";
 
-describe('FizzBuzz test for multiples of 3 and 5', () => {
-       
-        test('returns Fizz when multiple of 3', () => {
-            // Gherking test
-            /**
-             * Scenario: Número divisible por 3
-             * Given un número 9
-             * When el número es procesado
-             * Then se muestra "Fizz"
-             */
+describe("FizzBuzz test for multiples of 3 and 5", () => {
+  test("returns Fizz when multiple of 3", () => {
+    // Gherking test
+    /**
+     * Scenario: Número divisible por 3
+     * Given un número 9
+     * When el número es procesado
+     * Then se muestra "Fizz"
+     */
 
-            // Arrange: Preparar el escenario
-            let valor_entrada = 9;
-            let respuesta_esperada = {
-                status: "ok", // Código indicando éxito
-                message: "El número es divisible por 3", // Mensaje de validación
-                data: {
-                    input: 9,  // Número evaluado
-                    output: "Fizz" // Resultado esperado
-                }}
+    // Arrange: Preparar el escenario
+    let valor_entrada = 9;
+    let respuesta_esperada = {
+      message: "Fizz",
+      userNumber: 9,
+      result: 0,
+    };
 
-            // Act: ejecutar el escenario
-            let resultado = fizzBuzz(valor_entrada);
-        
-            // Assert: Comprobar el escenario
-            expect(typeof resultado.data.input).toBe("number"); // valida que input sea un número
-            expect(resultado).toEqual(respuesta_esperada); // valida estructura de respuesta
-            expect(resultado.data.output).toBe("Fizz"); // valida que el valor output corresponda al expect
-        });
+    // Act: ejecutar el escenario
+    let resultado = fizzBuzzLogic(valor_entrada);
 
-        test('returns Buzz when multiple of 5', () => {
-            //Gherking test
-            /**
-             * Scenario: Número divisible por 5
-             * Given un número 10
-             * When el número es procesado
-             * Then se muestra "Buzz"
-             */
+    // Assert: Comprobar el escenario
+    expect(resultado).toEqual(respuesta_esperada); // valida estructura de respuesta
+    expect(resultado.message).toBe("Fizz"); // valida que el valor output corresponda al expect
+  });
 
-            // Arrange
+  test("returns Buzz when multiple of 5", () => {
+    //Gherking test
+    /**
+     * Scenario: Número divisible por 5
+     * Given un número 10
+     * When el número es procesado
+     * Then se muestra "Buzz"
+     */
 
-            // Act
-    
-            // Assert
-    
-        })
+    // Arrange
+    let valor_entrada = 10;
+    let respuesta_esperada = {
+      message: "Buzz",
+      userNumber: 10,
+      result: 0,
+    };
 
-        test('returns FizzBuzz when multiple of 3 and 5', () => {
-            //Gherking test
-            /**
-             * Scenario: Número divisible por 3 y 5
-             * Given un número 15
-             * When el número es procesado
-             * Then se muestra "FizzBuzz"
-             */
-        })
+    // Act
+    let resultado = fizzBuzzLogic(valor_entrada);
 
-        test('returns number when is not multiple of 3 and 5', () => {
-            //Gherking test
-            /**
-             * Scenario: Número no divisible por 3 ni 5
-             * Given un número 8
-             * When el número es procesado
-             * Then se muestra el número ingresado
-             */
-        })
-    }
-)
+    // Assert
+    expect(resultado).toEqual(respuesta_esperada);
+    expect(resultado.message).toBe("Buzz");
+  });
+
+  test("returns FizzBuzz when multiple of 3 and 5", () => {
+    //Gherking test
+    /**
+     * Scenario: Número divisible por 3 y 5
+     * Given un número 15
+     * When el número es procesado
+     * Then se muestra "FizzBuzz"
+     */
+  });
+
+  test("returns number when is not multiple of 3 and 5", () => {
+    //Gherking test
+    /**
+     * Scenario: Número no divisible por 3 ni 5
+     * Given un número 8
+     * When el número es procesado
+     * Then se muestra el número ingresado
+     */
+  });
+});
